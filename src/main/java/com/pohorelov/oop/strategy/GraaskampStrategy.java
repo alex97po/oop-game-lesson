4f347b9eb2aa8c720f3526745afc92a20f68f659
Oleksandr Pohorelov <sas.pogorelov@gmail.com>
2026-04-20 12:33:01 +0200

    Implemented GraaskampStrategy.java

---
+++ src/main/java/com/pohorelov/oop/strategy/GraaskampStrategy.java
@@ -0,0 +1,22 @@
+package com.pohorelov.oop.strategy;
+
+import com.pohorelov.oop.decision.BetrayDecision;
+import com.pohorelov.oop.decision.CooperateDecision;
+import com.pohorelov.oop.decision.Decision;
+
+public class GraaskampStrategy extends Strategy {
+
+  // Same as Joss, but betrays not in 10%, but on each 50th round
+  @Override
+  public Decision decide(Decision[] yourDecisions, Decision[] opponentDecisions,
+                         int currentTurnIndex) {
+    if (currentTurnIndex == 0) {
+      return CooperateDecision.getInstance();
+    }
+    if (currentTurnIndex + 1 % 50 == 0) {
+      return BetrayDecision.getInstance();
+    }
+    return opponentDecisions[currentTurnIndex-1];
+  }
+
+}
