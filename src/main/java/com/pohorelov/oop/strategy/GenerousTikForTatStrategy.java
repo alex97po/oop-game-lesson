5b323a3799ba85236a99cb691cca303fd6a83f04
Denys <podenys54321@gmail.com>
2026-04-20 13:36:04 +0300

    Add GenerousTikForTat strategy

---
+++ src/main/java/com/pohorelov/oop/strategy/GenerousTikForTatStrategy.java
@@ -0,0 +1,28 @@
+package com.pohorelov.oop.strategy;
+
+import com.pohorelov.oop.decision.BetrayDecision;
+import com.pohorelov.oop.decision.CooperateDecision;
+import com.pohorelov.oop.decision.Decision;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GenerousTikForTatStrategy extends Strategy {
+
+  @Override
+  public Decision decide(Decision[] yourDecision,
+                         Decision[] opponentDecisions,
+                         int currentTurnIndex) {
+    if (currentTurnIndex == 0) {
+      return CooperateDecision.getInstance();
+    }
+
+    if (opponentDecisions[currentTurnIndex - 1] instanceof BetrayDecision) {
+      if (ThreadLocalRandom.current().nextInt(10) < 9) {
+        return BetrayDecision.getInstance();
+      }
+      return CooperateDecision.getInstance();
+    }
+
+    return CooperateDecision.getInstance();
+  }
+
+}
--- src/main/java/com/pohorelov/oop/strategy/StrategyRegistrar.java
+++ src/main/java/com/pohorelov/oop/strategy/StrategyRegistrar.java
@@ -3,7 +3,8 @@ package com.pohorelov.oop.strategy;
 public class StrategyRegistrar {
 
   private static final Strategy[] STRATEGIES = new Strategy[] {
-      new SampleStrategy()
+      new SampleStrategy(),
+      new GenerousTikForTatStrategy()
   };
 
   public static Strategy[] getStrategies() {
